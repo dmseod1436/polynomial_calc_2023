bcc8fe24da57fc71668272635e5d6f0367bcf159
dmswldms121 <dmswldms12@gmail.com>
2023-03-14 16:56:11 +0900

    compound arithmetic(4)

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -1,25 +1,43 @@
 package com.ll;
+
 public class Calc {
     public static int run(String exp) {
         exp = exp.replaceAll("- ", "+ -");
+        boolean needToMulti = exp.contains("*");
+        boolean needToPlus = !needToMulti;
 
         String[] bits = exp.split(" \\+ ");
+        if (needToPlus) {
+            exp = exp.replaceAll("- ", "+ -");
 
-        int a = Integer.parseInt(bits[0]);
-        int b = Integer.parseInt(bits[1]);
-        int c = 0;
-        int sum = 0;
+            int sum = 0;
+            String[] bits = exp.split(" \\+ ");
 
-        if (bits.length > 2) {
-            c = Integer.parseInt(bits[2]);
             for (int i = 0; i < bits.length; i++) {
                 sum += Integer.parseInt(bits[i]);
+                int sum = 0;
+
+                for (int i = 0; i < bits.length; i++) {
+                    sum += Integer.parseInt(bits[i]);
+                }
+
+                return sum;
+            }
+        else if (needToMulti) {
+                String[] bits = exp.split(" \\* ");
+
+                int sum = 1;
+
+                return sum;
+                for (int i = 0; i < bits.length; i++) {
+                    sum *= Integer.parseInt(bits[i]);
+                }
+
+                return sum;
             }
 
-            return a + b + c;
-            return sum;
-
             // throw new RuntimeException("올바른 계산식이 아닙니다.");
+            throw new RuntimeException("올바른 계산식이 아닙니다.");
         }
     }
 }
