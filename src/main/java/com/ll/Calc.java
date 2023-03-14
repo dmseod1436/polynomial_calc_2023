f336d80c6ea972b24691ca85ec619c79a86d2b35
dmswldms121 <dmswldms12@gmail.com>
2023-03-14 17:04:16 +0900

    bracket modification

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -1,16 +1,17 @@
 package com.ll;
 import java.util.Arrays;
 import java.util.stream.Collectors;
+
 public class Calc {
     public static int run(String exp) {
+        exp = stripOuterBrackets(exp);
+
         // 단일항이 입력되면 바로 리턴
         if ( !exp.contains(" ") ) return Integer.parseInt(exp);
 
         boolean needToMulti = exp.contains(" * ");
         boolean needToPlus = exp.contains(" + ") || exp.contains(" - ");
-
         boolean needToCompound = needToMulti && needToPlus;
-
         if ( needToCompound ) {
             String[] bits = exp.split(" \\+ ");
             String newExp = Arrays.stream(bits)
@@ -36,6 +37,15 @@ public class Calc {
             }
             return sum;
         }
+
         throw new RuntimeException("올바른 계산식이 아닙니다.");
+    }
+
+    private static String stripOuterBrackets(String exp) {
+        if ( exp.charAt(0) == '(' && exp.charAt(exp.length() - 1) == ')' ) {
+            exp = exp.substring(1, exp.length() - 1);
+        }
+
+        return exp;
     }
 }
