7f3b4bfd9ed874b351277bc594f2422a855bccb6
dmswldms121 <dmswldms12@gmail.com>
2023-03-14 16:59:28 +0900

    compound arithmetic(6)

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -1,35 +1,45 @@
 package com.ll;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Calc {
     public static int run(String exp) {
+        // 단일항이 입력되면 바로 리턴
+        if ( !exp.contains(" ") ) return Integer.parseInt(exp);
+
         boolean needToMulti = exp.contains("*");
-        boolean needToPlus = !needToMulti;
         boolean needToPlus = exp.contains("+");
 
-        if (needToPlus) {
-            boolean needToCompound = needToMulti && needToPlus;
+        boolean needToCompound = needToMulti && needToPlus;
+        if ( needToCompound ) {
+            String[] bits = exp.split(" \\+ ");
 
-            if (needToCompound) {
-                String[] bits = exp.split(" \\+ ");
+            return Integer.parseInt(bits[0]) + run(bits[1]);
+            String newExp = Arrays.stream(bits)
+                    .mapToInt(Calc::run)
+                    .mapToObj(e -> e + "")
+                    .collect(Collectors.joining(" + "));
 
-                return Integer.parseInt(bits[0]) + run(bits[1]);
-            } else if (needToPlus) {
-                exp = exp.replaceAll("- ", "+ -");
-
-                String[] bits = exp.split(" \\+ ");
-                int sum = 0;
-                for (int i = 0; i < bits.length; i++) {
-                    sum += Integer.parseInt(bits[i]);
-                }
-                return sum;
-            } else if (needToMulti) {
-                String[] bits = exp.split(" \\* ");
-                int sum = 1;
-                for (int i = 0; i < bits.length; i++) {
-                    sum *= Integer.parseInt(bits[i]);
-                }
-                return sum;
-            }
-            throw new RuntimeException("올바른 계산식이 아닙니다.");
+            return run(newExp);
         }
+        else if ( needToPlus ) {
+            exp = exp.replaceAll("- ", "+ -");
+            String[] bits = exp.split(" \\+ ");
+            int sum = 0;
+            for (int i = 0; i < bits.length; i++) {
+                sum += Integer.parseInt(bits[i]);
+            }
+            return sum;
+        }
+        else if ( needToMulti ) {
+            String[] bits = exp.split(" \\* ");
+            int sum = 1;
+            for (int i = 0; i < bits.length; i++) {
+                sum *= Integer.parseInt(bits[i]);
+            }
+            return sum;
+        }
+        throw new RuntimeException("올바른 계산식이 아닙니다.");
     }
 }
